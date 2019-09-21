b65921fd0a8047dee15968ca5710b29316d322ca
prathamzx <pratham.1999@yahoo.in>
2019-09-21 18:30:36 +0530

    Form pages added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Route, HashRouter, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Onboard from './components/Onboard';
+import OnboardDoctor from './components/OnboardDoctor';
+import OnboardHospital from './components/OnboardHospital';
+import Patient from './components/Patient'
 
 function App() {
   return (
@@ -8,6 +12,11 @@ function App() {
     <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path='/' component={Home} />{' '}
+          <Route path='/onboard/Hospital/Patient' component={Patient} />{' '}
+          <Route path='/onboard/Doctor' component={OnboardDoctor} />{' '}
+          <Route path='/onboard/Hospital' component={OnboardHospital} />{' '}
+          <Route path='/onboard' component={Onboard} />{' '}
+
         </Switch>{' '}
       </HashRouter>{' '}
     </div>
---
+++ src/components/OnboardDoctor.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from 'react';
+import { Button, Form, Container } from 'semantic-ui-react'
+// import Context from '../contextStore/Context';
+// import axios from 'axios';
+// import { Redirect , Link } from 'react-router-dom';
+const OnboardDoctor = props => {
+  const [formData, setFormData] = useState({
+      specialization: '',
+      phone: ''
+    });
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+const onSubmit=()=>{
+  console.log(formData);
+};
+
+
+  return(
+    <>
+    <Container>
+      <h1>Doctor</h1>
+      <br/>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+            <label htmlFor='phone'>Contact Number :</label>
+            <div className='form-item'>
+              <input
+                name='phone'
+                onChange={handleChange}
+                type='text'
+                maxLength='10'
+                required='required'
+              />
+            </div>
+            </Form.Field>
+            <Form.Field>
+            <label htmlFor='specialization'>Specialization</label>
+            <div className='form-item'>
+              <select name='specialization' onChange={handleChange} required='required'>
+                <option value='Disease1'>Disease 1</option>
+                <option value='Disease2'>Disease 2</option>
+                <option value='Disease3'>Disease 3</option>
+                <option value='Disease4'>Disease 4</option>
+              </select>
+            </div>
+            </Form.Field>
+            <Button>Send</Button>
+          </Form>
+        </Container>
+    </>
+  );
+};
+
+export default OnboardDoctor;
