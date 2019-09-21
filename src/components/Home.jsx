f11b0089693b230513ec17fa9666b2e36e950377
saranonearth <saran.suresh@outlook.com>
2019-09-21 21:32:52 +0530

    context store update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { Link, Redirect } from 'react-router-dom';
 import Context from '../contextStore/Context';
 import config from '../config.json';
 import axios from 'axios';
 const Home = props => {
   const state = useContext(Context);
-  const responseGoogle = response => {
-    console.log(response);
-  };
-
-  if (state.user && !state.user.onBoard) {
-    props.history.push('/onboard');
-  }
 
   const onsuccess = async user => {
     try {
@@ -26,11 +18,14 @@ const Home = props => {
 
       const res = await axios.post('http://localhost:4000/auth', body, config);
       console.log(res);
-    } catch (error) {}
+    } catch (error) {
+      console.log('auth error');
+    }
   };
   const onfailure = () => {
     console.log('failure');
   };
+
   return (
     <div className=''>
       <h1>Home</h1>
